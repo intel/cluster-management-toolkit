4ccf62a6f44850d2c78ce82754f14e43b307e530
David Weinehall <david.weinehall@linux.intel.com>
2022-01-18 22:40:43 +0200

    ikt: Add purge command
    Just like iktadm purge-control-plane can be used to purge
    leftover Kubernetes configuration and software from a control plane,
    ikt purge can purge a host that has had previously served as a node.
    
    Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>

--- about.py
+++ about.py
@@ -1,17 +1,17 @@
 #! /usr/bin/env python3
 
-copyright = "Copyright © 2019-2021 Intel Corporation"
+copyright = "Copyright © 2019-2022 Intel Corporation"
 license = "This is free software; see the source for copying conditions.  There is NO\nwarranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
 
 program_suite_name = "iKT"
 program_suite_full_name = "Intel Kubernetes Toolkit"
-program_suite_version = "0.1"
+program_suite_version = "0.1.1"
 
 ui_program_name = "iku"
-ui_program_version = "0.1.2"
+ui_program_version = "0.1.3"
 
 tool_program_name = "ikt"
-tool_program_version = "0.1.2"
+tool_program_version = "0.1.3"
 
 install_program_name = "ikt-install"
 install_program_version = "0.1"
