4cae90697edaeae40b38ee3d2779e1f17e59e1b0
David Weinehall <david.weinehall@linux.intel.com>
2022-02-25 13:05:36 +0200

    iku: Fix container version# extractor
    In some cases the extraction of the container version# failed.
    
    Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>

--- kubernetes_helper.py
+++ kubernetes_helper.py
@@ -316,11 +316,13 @@ class KubernetesHelper:
 		return ",".join(selectors)
 
 	def get_image_version(self, image, default = "<undefined>"):
-		tmp = re.match(r"^.*:(.*)", image)
-		if tmp is not None:
-			image_version = f"{tmp[1]}"
-		else:
-			image_version = default
+		image_version = default
+		regexes = [r"^.+/[^:^/]+:(.+?)@.*", r"^.+/[^:^/]+:(.+)"]
+		for _regex in regexes:
+			tmp = re.match(_regex, image)
+			if tmp is not None:
+				image_version = f"{tmp[1]}"
+				break
 		return image_version
 
 	# CNI detection helpers
