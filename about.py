intel/cluster-management-toolkit

commit 1112a9ecfefdf26dd8d238fac2bbd71cb3867722

ikt-install: Add --no-dependencies option
There's currently no support for installing IKT's dependencies
on non-Debian (and derivative) systems; add an option that simply
skips this step. This allows for the user to install the dependencies
manually.

Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>

about.py
@@ -14,7 +14,7 @@ tool_program_name = "ikt"
 tool_program_version = "0.1.7"
 
 install_program_name = "ikt-install"
-install_program_version = "0.2"
+install_program_version = "0.3"
 
 admin_program_name = "iktadm"
 admin_program_version = "0.1.7"

ikt-install
@@ -96,7 +96,6 @@ packages = [
 	"python3-paramiko",
 	"python3-ujson",
 	"python3-urllib3",
-	"sshpass",
 ]
 
 def install_software(verbose = False):
@@ -105,17 +104,22 @@ def install_software(verbose = False):
 
 def install(options = [], args = None):
 	verbose = False
+	install_dependencies = True
 
 	for opt, optarg in options:
 		if opt == "--verbose":
 			verbose = True
+		elif opt == "--no-dependencies":
+			install_dependencies = False
 		else:
 			raise Exception(f"Programming error; invalid option {opt}")
 
 	create_directories(verbose = verbose)
 	copy_configuration_files(verbose = verbose)
 	create_symlinks(verbose = verbose)
-	install_software(verbose = verbose)
+
+	if install_dependencies == True:
+		install_software(verbose = verbose)
 
 commandline = {
 	# Global option
@@ -126,6 +130,9 @@ commandline = {
 			"--verbose": {
 				"description": [("Be more verbose", "description")],
 			},
+			"--no-dependencies": {
+				"description": [("Do not install dependencies", "description")],
+			},
 		},
 		"min_args": 0,
 		"max_args": 0,
@@ -135,6 +142,11 @@ commandline = {
 	"__default": {
 		"command": ["__default"],
 		"description": [("", "")],
+		"options": {
+			"--no-dependencies": {
+				"description": [("Do not install dependencies", "description")],
+			},
+		},
 		"min_args": 0,
 		"max_args": 0,
 		"callback": install,

commit 15b0c91e9b1bc68c3a5335ad3c3d86c472245aaa

ikt-install: Install sshpass again
Commit 1112a9ecfefdf26dd8d238fac2bbd71cb3867722 mistakenly removed
the line to install sshpass; restore it, but keep the rest of the
changes from that commit intact.

Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>

about.py
@@ -14,7 +14,7 @@ tool_program_name = "ikt"
 tool_program_version = "0.1.8"
 
 install_program_name = "ikt-install"
-install_program_version = "0.3"
+install_program_version = "0.4"
 
 admin_program_name = "iktadm"
 admin_program_version = "0.1.8"

ikt-install
@@ -96,6 +96,7 @@ packages = [
 	"python3-paramiko",
 	"python3-ujson",
 	"python3-urllib3",
+	"sshpass",
 ]
 
 def install_software(verbose = False):