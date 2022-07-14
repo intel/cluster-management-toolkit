af0f24eb91fdaea76bd3dc591ed46fbe4b13f892
David Weinehall <tao@debian.org>
2022-07-14 21:59:08 +0300

    ikt: Add support for passing hosts from a file
    By using the --from-file option the argument to prepare and add-nodes
    will be treated as a path to a newline separated file of hostnames.
    
    This is a significantly better solution when adding many nodes at once.
    
    Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>

--- about.py
+++ about.py
@@ -11,7 +11,7 @@ ui_program_name = "iku"
 ui_program_version = "0.2.1"
 
 tool_program_name = "ikt"
-tool_program_version = "0.1.8"
+tool_program_version = "0.1.9"
 
 install_program_name = "ikt-install"
 install_program_version = "0.4"
