a80c9fec65828fe442bb8464aee82f9eef359abe
David Weinehall <david.weinehall@linux.intel.com>
2022-09-29 19:38:23 +0300

    iktadm,iktio: Verify checksums if available
    If there's a checksum available for a file to download we might
    as well verify it (even if downloading a checksum from the same
    location that we download the file from isn't really much
    of a security measure, since anyone subverting one would obviously
    subvert the other too).
    
    Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>

--- iktio.py
+++ iktio.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 # Requires: python3 (>= 3.6)
 from functools import partial
+import hashlib
 import os
+import re
 import sys
 import tarfile
 import tempfile
@@ -16,10 +18,77 @@ from iktlib import deep_get
 
 from iktprint import iktprint
 
+def verify_checksum(checksum, checksum_type, data, filename = None):
+	if checksum_type is None:
+		return True
+
+	if checksum_type in ["md5"]:
+		m = hashlib.md5()
+	elif checksum_type in ["sha", "sha1"]:
+		m = hashlib.sha1()
+	elif checksum_type in ["sha224"]:
+		m = hashlib.sha224()
+	elif checksum_type in ["sha256"]:
+		m = hashlib.sha256()
+	elif checksum_type in ["sha384"]:
+		m = hashlib.sha384()
+	elif checksum_type in ["sha512"]:
+		m = hashlib.sha512()
+	elif checksum_type in ["blake2b"]:
+		m = hashlib.blake2b()
+	elif checksum_type in ["blake2s"]:
+		m = hashlib.blake2s()
+	elif checksum_type in ["sha3"]:
+		m = hashlib.sha3()
+	elif checksum_type in ["sha3_224"]:
+		m = hashlib.sha3_224()
+	elif checksum_type in ["sha3_256"]:
+		m = hashlib.sha3_256()
+	elif checksum_type in ["sha3_384"]:
+		m = hashlib.sha3_384()
+	elif checksum_type in ["sha3_512"]:
+		m = hashlib.sha3_512()
+	elif checksum_type in ["shake_128"]:
+		m = hashlib.shake_128()
+	elif checksum_type in ["shake_256"]:
+		m = hashlib.shake_256()
+	else:
+		return False
+
+	m.update(data)
+
+	# If filename is supplied it's expected that the checksum file can contain
+	# more than one checksum, or at least that it contains a filename;
+	# if so we find the matching entry
+	regex = re.compile(r"^([0-9a-f]+)\s+(\S+)$")
+	match_checksum = None
+
+	for line in checksum.decode("utf-8").splitlines():
+		if filename is not None:
+			tmp = regex.match(line)
+			if tmp is not None:
+				if tmp[2] != filename:
+					continue
+				match_checksum = tmp[1]
+				break
+		else:
+			match_checksum = line
+			break
+
+	if match_checksum is None:
+		return False
+	elif m.hexdigest() != match_checksum:
+		return False
+
+	return True
+
 # download_files can extract single files from archives; it will not extract entire archives due to the security risks,
 # and it requires the full path of the file within the archive to be specified.
 # If later necessary this function could be modified to take a list of multiple files to extract from one tarball;
 # for now this does what is necessary though.
+#
+# fetch_urls is a list of tuples:
+# (URL to file or archive, file to extract, URL to checksum, type of checksum)
 def download_files(directory, fetch_urls, permissions = 0o644):
 	iktconfig = iktlib.read_iktconfig()
 
@@ -36,27 +105,42 @@ def download_files(directory, fetch_urls, permissions = 0o644):
 	else:
 		spm = urllib3.PoolManager()
 
-	for url in fetch_urls:
-		if type(url) == tuple:
-			url, filename = url
-		else:
-			tmp = re.match(r".*/(.*)", url)
-			if tmp is None:
-				iktprint([("Error: ", "error"), ("Could not extract filename from URL ", "default"), (f"{url}", "url")], stderr = True)
+	for url, filename, checksum_url, checksum_type in fetch_urls:
+		# If there's a checksum file, download it first
+		checksum = None
+
+		if checksum_url is not None:
+			if checksum_url.startswith("http"):
+				r1 = pm.request("GET", checksum_url)
+			elif checksum_url.startswith("https"):
+				r1 = spm.request("GET", checksum_url)
+			else:
+				iktprint([("Error:", "error"), (" Unknown or missing protocol; Checksum URL ", "description"), (f"{checksum_url}", "url")], stderr = True)
 				retval = False
-				continue
-			filename = tmp[1]
+				break
+
+			if r1.status == 200:
+				checksum = r1.data
+			else:
+				retval = False
+				break
 
 		if url.startswith("http"):
 			r1 = pm.request("GET", url)
 		elif url.startswith("https"):
 			r1 = spm.request("GET", url)
 		else:
-			iktprint([("Error: ", "error"), ("Unknown or missing protocol; URL ", "description"), (f"{url}", "url")], stderr = True)
+			iktprint([("Error:", "error"), (" Unknown or missing protocol; URL ", "description"), (f"{url}", "url")], stderr = True)
 			retval = False
 			continue
 
 		if r1.status == 200:
+			# If we have a checksum we need to confirm that the downloaded file matches the checksum
+			if checksum is not None and verify_checksum(checksum, checksum_type, r1.data, os.path.basename(url)) == False:
+				iktprint([("Critical:", "error"), (" File downloaded from ", "description"), (f"{url}", "url"), (" did not match its expected checksum; aborting.", "description")], stderr = True)
+				retval = False
+				break
+
 			dl = tempfile.NamedTemporaryFile(delete = False)
 			with open(dl.name, "wb", opener = partial(os.open, mode = 0o600)) as f:
 				try:
