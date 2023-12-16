505d606cfc62a49a7e2908e8bea9a858a98c1052
David Weinehall <david.weinehall@gmail.com>
2023-12-16 23:15:29 +0200

    cmtinv: Better parameter checking for playbooks
    Don't silently abort on non-existing playbooks;
    tell the user why they were ignored instead,
    and show how to list the valid playbooks.
    
    Signed-off-by: David Weinehall <david.weinehall@gmail.com>

--- about.py
+++ about.py
@@ -28,7 +28,7 @@ ADMIN_PROGRAM_NAME = "cmtadm"
 ADMIN_PROGRAM_VERSION = "0.8.3"
 
 INVENTORY_PROGRAM_NAME = "cmtinv"
-INVENTORY_PROGRAM_VERSION = "0.4.3"
+INVENTORY_PROGRAM_VERSION = "0.4.4"
 
 # We don't support Python-versions older than 3.8
 version_info = sys.version_info
