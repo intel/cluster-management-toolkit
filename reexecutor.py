e1e65c5a58ec066114ebd2c549f88b105adc11c4
David Weinehall <david.weinehall@intel.com>
2024-01-25 16:23:02 +0200

    cmu: Fix asynchronous Inventory behaviour
    We now detect addition and deletion of hosts.
    Rather than just remove them from the list they'll
    be marked as deleted and dimmed.
    
    The next step is to do the same with all asynchronous list
    items, and trigger removal only when forcing update [F5],
    and to make deleted items unselectable (take care to handle
    the special case where none of the entries are selectable).
    
    Signed-off-by: David Weinehall <david.weinehall@intel.com>

--- reexecutor.py
+++ reexecutor.py
@@ -2,7 +2,7 @@
 # Requires: python3 (>= 3.8)
 
 """
-A wrapper for using futures
+A wrapper for using for executing callables asynchronously using the concurrent.futures ThreadPoolExecutor
 """
 
 import concurrent.futures
@@ -12,7 +12,16 @@ import threading
 from typing import Any, Callable, List, Optional, Tuple, Type
 
 class ReExecutor:
+	"""
+	A wrapper class for concurrent futures
+	"""
 	def __init__(self, max_workers: Optional[int] = None) -> None:
+		"""
+		Init method for the ReExecutor() class
+
+			Parameters:
+				max_workers (int): The maximum number of worker threads to dimension
+		"""
 		if max_workers is not None:
 			self.executor = concurrent.futures.ThreadPoolExecutor(max_workers = max_workers)
 		else:
@@ -21,12 +30,30 @@ class ReExecutor:
 		self.lock = threading.Lock()
 
 	def __trigger(self, key: str) -> None:
+		"""
+		Internal method for (re-)triggering a callable
+
+			Parameter:
+				key (str): The identifier for the callable to (re-)trigger
+		"""
 		data = self.futures[key]
 		data["triggered"] = True
+		if data["future"] is not None:
+			data["future"].cancel()
 		data["future"] = self.executor.submit(data["function"], *data["args"], **data["kwargs"])
 		self.futures[key] = data
 
 	def trigger(self, key: str, interval: int, fn: Callable, /, *args, **kwargs) -> None:
+		"""
+		Trigger a callable
+
+			Parameter:
+				key (str): The identifier for the future to (re-)trigger
+				interval (int): Interval between each retrigger, or -1 to only run once
+				fn (callable): The callable to execute
+				*args (list): Positional arguments to pass to fn
+				**kwargs (dict): Keyword arguments to pass to fn
+		"""
 		with self.lock:
 			if key not in self.futures:
 				data = {
@@ -41,9 +68,37 @@ class ReExecutor:
 			self.__trigger(key)
 
 	def retrigger(self, key: str) -> None:
+		"""
+		Retrigger a callable
+
+			Parameter:
+				key (str): The identifier for the callable to (re-)trigger
+		"""
 		self.trigger(key, self.futures[key]["interval"], self.futures[key]["function"], self.futures[key]["args"], self.futures[key]["kwargs"])
 
+	def update(self, key: str, **kwargs) -> None:
+		"""
+		Update one or several parameters for a callable; we currently only support updating keyword args
+
+			Parameters:
+				key (str): The identifier for the callable to update parameters for
+				kwargs (dict): The variables to update
+		"""
+		with self.lock:
+			if key in self.futures:
+				for kwarg, value in kwargs.items():
+					self.futures[key]["kwargs"][kwarg] = value
+				self.__trigger(key)
+
 	def get(self, key: str) -> Tuple[List[Type], List[Any]]:
+		"""
+		Check if there's available data from a callable; if there is, return it
+
+			Parameters:
+				key (str): The identifier for the callable to update parameters for
+			Returns:
+				([Type], [Any]): (data, status) or ([], []) if no data is available
+		"""
 		info = []
 		status = []
 
@@ -53,16 +108,18 @@ class ReExecutor:
 				info, status = self.futures[key]["future"].result()
 				self.futures[key]["triggered"] = False
 				self.futures[key]["future"] = None
+			elif self.futures[key]["interval"] == -1:
+				pass
 			elif not self.futures[key]["triggered"] and (datetime.now() - self.futures[key]["last_update"]).seconds > self.futures[key]["interval"]:
 				self.retrigger(key)
 		return info, status
 
 	def delete(self, key: str) -> None:
 		"""
-		Delete a future
+		Delete a callable
 
 			Parameters:
-				key (str): The future to delete
+				key (str): The callable to delete
 		"""
 		with self.lock:
 			if key in self.futures:
@@ -71,6 +128,9 @@ class ReExecutor:
 				self.futures.pop(key)
 
 	def flush(self) -> None:
+		"""
+		Flush the list of callables
+		"""
 		for key in list(self.futures.keys()):
 			self.delete(key)
 
