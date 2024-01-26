eae5df7e726eceb60acfd2fd24373ea5fbbdf545
David Weinehall <david.weinehall@gmail.com>
2024-01-26 02:08:14 +0200

    Minor cleanup
    Remove some unused imports, etc.
    
    Signed-off-by: David Weinehall <david.weinehall@gmail.com>

--- kubernetes_helper.py
+++ kubernetes_helper.py
@@ -43,7 +43,7 @@ import cmtlib
 from cmtlib import datetime_to_timestamp, get_since, timestamp_to_datetime, versiontuple
 #from cmtlog import debuglog
 #from cmttypes import LogLevel
-from cmttypes import deep_get, deep_get_with_fallback, deep_set, DictPath, FilePath, FilePathAuditError, SecurityChecks, SecurityPolicy, StatusGroup
+from cmttypes import deep_get, deep_get_with_fallback, DictPath, FilePath, FilePathAuditError, ProgrammingError, SecurityChecks, SecurityPolicy, StatusGroup
 from cmtio import execute_command_with_response, secure_which
 from cmtio import secure_read
 from cmtio_yaml import secure_read_yaml, secure_write_yaml
