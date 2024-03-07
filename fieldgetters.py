8b9f8747f78ec781985a9b7f82c9b7f1365062e7
David Weinehall <david.weinehall@gmail.com>
2024-03-07 10:59:46 +0200

    *: Lots and lots of code cleanup
    Continue the path of code cleanup.  Start reformatting
    docstrings to be a.) more uniform, b.) indented with spaces
    instead of with tabs.
    
    Signed-off-by: David Weinehall <david.weinehall@gmail.com>

---
+++ fieldgetters.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+#
+# Copyright the Cluster Management Toolkit for Kubernetes contributors.
+# SPDX-License-Identifier: MIT
+
+"""
+Get data for fields in a list; typically used to populate _extra_data
+"""
+
+import copy
+from typing import Any, List
+
+from cmttypes import deep_get, DictPath, ProgrammingError
+
+def fieldgetter_api_server_version(**kwargs: Any) -> List[Any]:
+	"""
+	A fieldgetter that provides the version
+	of the Kubernetes API-server
+
+	    Parameters:
+	        **kwargs (dict[str, Any]): Keyword arguments
+	            kubernetes_helper (KubernetesHelper): A reference to a KubernetesHelper object
+	            fields ([int]): The indexes of the API-server version fields to return
+	    Returns:
+	        [str]: The list of API-server version fields
+	"""
+	if (kh := deep_get(kwargs, DictPath("kubernetes_helper"))) is None:
+		raise ProgrammingError("fieldgetter_api_server_version() called without kubernetes_helper")
+	fields: List[Any] = deep_get(kwargs, DictPath("fields"), [])
+
+	field_list = []
+	result = kh.get_api_server_version()
+	if not fields:
+		field_list = list(copy.deepcopy(result))
+	else:
+		for i, field in enumerate(result):
+			if i in fields:
+				field_list.append(field)
+	return field_list
+
+
+# Fieldgetters acceptable for direct use in view files
+fieldgetter_allowlist = {
+	"fieldgetter_api_server_version": fieldgetter_api_server_version,
+}
